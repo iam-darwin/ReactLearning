df07a0a398ff94e518d49f0c159c6b78fa30e819
JO3Y DSVS <darushyam143@gmail.com>
2023-11-16 17:50:10 +0530

    noting

--- 02react/src/App.jsx
+++ 02react/src/App.jsx
@@ -1,47 +1,77 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState, useRef } from 'react';
+import './App.css';
 
 function App() {
-  let [password,setPassword]=useState("");
-  let [number,setNumber]=useState(false);
-  let [chars,setChars]=useState(false);
-  let [length,setLength]=useState(8);
+  const [password, setPassword] = useState('');
+  const [number, setNumber] = useState(false);
+  const [chars, setChars] = useState(false);
+  const [length, setLength] = useState(8);
+  const passwordInputRef = useRef(null);
 
-  const pwdGen=()=>{
-    let pwd="";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const pwdGen = () => {
+    let pwd = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-    if (number) str += "0123456789"
-    if (chars) str += "!@#$%^&*-_+=[]{}~`"
+    if (number) str += '0123456789';
+    if (chars) str += '!@#$%^&*-_+=[]{}~`';
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-      pwd += str.charAt(char)
-      
+      let char = Math.floor(Math.random() * str.length);
+      pwd += str.charAt(char);
     }
     setPassword(pwd);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     pwdGen();
-  },[number,chars,length,setPassword])
+  }, [number, chars, length]);
 
-  return(
-  <>
-    <h1>Password generator</h1>
-    <input type="text" value={password}/> <label>Password</label>
-    <br />
-    <input type="range" min="8" max="100" onChange={(e)=>setLength(e.target.value)} /><label>{length}</label>
-    <br />
-    <input type="checkbox" id="myCheckbox" name="myCheckbox" onChange={()=>setNumber(!number)}></input>
-    <label for="myCheckbox">Numbers</label>
-    <br />
-    <input type="checkbox" id="myCheckbox" name="myCheckbox" onChange={()=>setChars(!chars)}></input>
-    <label for="myCheckbox">Charas</label>
-  </>
-    
-  )
+  const handleCopyClick = () => {
+    if (passwordInputRef.current) {
+      passwordInputRef.current.select();
+      document.execCommand('copy');
+    }
+  };
+
+  return (
+    <>
+      <h1>Password generator</h1>
+      <input
+        type="text"
+        value={password}
+        ref={passwordInputRef}
+      />
+      <label>Password</label>
+      <button onClick={handleCopyClick}>Copy</button>
+      <br />
+      <input
+        type="range"
+        min="8"
+        max="20"
+        value={length}
+        onChange={(e) => setLength(e.target.value)}
+      />
+      <label>{length}</label>
+      <br />
+      <input
+        type="checkbox"
+        id="numbersCheckbox"
+        name="numbersCheckbox"
+        checked={number}
+        onChange={() => setNumber(!number)}
+      />
+      <label htmlFor="numbersCheckbox">Numbers</label>
+      <br />
+      <input
+        type="checkbox"
+        id="charsCheckbox"
+        name="charsCheckbox"
+        checked={chars}
+        onChange={() => setChars(!chars)}
+      />
+      <label htmlFor="charsCheckbox">Characters</label>
+    </>
+  );
 }
 
-export default App
+export default App;
